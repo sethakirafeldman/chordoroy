2724f8136476934e24c6ffa0e9943d10d192e3d6
namdlefhtes <namdlefhtes@gmail.com>
2022-09-17 17:47:11 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const main = document.getElementById("main");
+
+const major = ["I", "ii", "iii", "IV", "V", "vi", "vii*"];
+
+let numDisplay = document.createElement("h1");
+main.appendChild(numDisplay)
+
+const randomChord = (type) => {
+    let randomNum = Math.floor(Math.random() * 7);
+    numDisplay.innerText = major[randomNum];
+    setTimeout(() => {
+        console.log(`test`)
+        randomChord(major);
+    }, 4000);
+}
+
+
+randomChord(major);
